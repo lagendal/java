cdbeaa62ed5a8d0d01559163abe4cf99e5183555
lagendal <lagendal1@gmail.com>
2018-02-16 02:59:37 +0900

    initial commit

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 13, b = 7, c,d = b/a;
+	float f = 20/8.f;
+	c = (a + b)*(a - b)-25;
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(f);
+    }
+}
